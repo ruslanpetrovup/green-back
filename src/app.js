e016ebd3bb38dda807e4f78f7d81023663f7d109
appitionitsolution <applicationitsolution@gmail.com>
2022-08-23 11:29:40 +0300

    test

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGO_DB).then(() => {
   console.log("connect db");
-  server.listen(6000, () => {
-    console.log("Server start port: 6000");
+  server.listen(3000, () => {
+    console.log("Server start port: 3000");
   });
 });
