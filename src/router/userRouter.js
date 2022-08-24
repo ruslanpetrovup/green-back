08704e10c2837acb3ef143ea63c4edbc91050952
appitionitsolution <applicationitsolution@gmail.com>
2022-08-24 12:09:02 +0300

    added update name

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -34,6 +34,23 @@ router.post("/login", async (req, res) => {
   }
 });
 
+router.post("/edit-name", async (req, res) => {
+  const { token, name, secondname } = req.body;
+  if (token === undefined) {
+    return res.send({ code: 400, body: "Not enough arguments" });
+  }
+  const { TOKEN_KEY } = process.env;
+  const { id } = jwt.verify(token, TOKEN_KEY);
+  const result = await SchemaUser.findById({ _id: id });
+
+  const total = await SchemaUser.findByIdAndUpdate(result.id, {
+    name: name === "" ? result.name : name,
+    secondname: secondname === "" ? result.secondname : secondname,
+  });
+  console.dir(name);
+  res.status(201);
+  res.send(total);
+});
 router.post("/registration", async (req, res) => {
   const { email, name, secondname, password } = req.body;
   try {
